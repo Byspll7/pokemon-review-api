51d75800a52e47913042de08378657ef9b5ce490
Byspll7 <148060095+Byspll7@users.noreply.github.com>
2025-01-12 19:44:51 +0400

    Add project files.

---
+++ PokemonReviewApp/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        ICollection<Category> GetCategories();
+        Category GetCategory(int ID);
+        ICollection<Pokemon> GetPokemonsByCategory(int categoryId);
+        bool CategoryExsists(int id);
+        bool CreateCategory(Category catgeory);
+        bool UpdateCategory(Category category);
+        bool DeleteCategory(Category category);
+        bool Save();
+    }
+}
